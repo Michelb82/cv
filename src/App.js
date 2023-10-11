7d27a629ccd502474e6f2568552b9497a2a0d15b
Michel Brand <michel.brand82@gmail.com>
2023-10-11 22:35:41 +0200

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
-import logo from './logo.svg';
 import './App.css';
+import CvNav from './components/CvNav';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <CvNav />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
---
+++ src/components/CvNav.js
@@ -0,0 +1,7 @@
+export default function CvNav() {
+    return (
+        <ul>
+            <li>test</li>
+        </ul>
+    );
+}
